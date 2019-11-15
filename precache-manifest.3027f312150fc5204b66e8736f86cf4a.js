6d08fa3f8ea023636a0e691b95f2293b2ccc5fba
lerato <leratorabotapi@hotmail.com>
2019-11-15 08:09:45 +0200

    Updates

---
+++ precache-manifest.3027f312150fc5204b66e8736f86cf4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15922b2ccbef11c55e94",
+    "url": "/weatherapp/static/js/runtime~main.15922b2c.js"
+  },
+  {
+    "revision": "2f93290620e8524e8b04",
+    "url": "/weatherapp/static/js/main.2f932906.chunk.js"
+  },
+  {
+    "revision": "208ea23f1b20fcf23dd4",
+    "url": "/weatherapp/static/js/2.208ea23f.chunk.js"
+  },
+  {
+    "revision": "2f93290620e8524e8b04",
+    "url": "/weatherapp/static/css/main.b218bcf5.chunk.css"
+  },
+  {
+    "revision": "aa55dd55065f297bfe29eaccfeca9a71",
+    "url": "/weatherapp/index.html"
+  }
+];
